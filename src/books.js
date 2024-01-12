7d99d9e50beb5daad4025ea3bed94cce3d8e0639
Hrushikesh Surkar <112410741+HrushikeshSurkar@users.noreply.github.com>
2024-01-12 08:24:22 +0530

    initial setup

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71CX11nGhWL._AC_UL600_SR600,400_.jpg',
+    title: 'Liz Cheney',
+    author: 'Oath and Honor',
+    id: 1,
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg',
+    title: 'James Clear',
+    author: 'Atomic Habits',
+    id: 2,
+  },
+];
